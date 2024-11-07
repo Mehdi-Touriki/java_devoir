ed06272c09a2fd1555e5858fd74dd8a426d5c47a
MeAny <rottenlpkd@proton.me>
2024-11-07 17:00:36 +0100

    confusing question 3!

--- devoir1/src/main/java/com/devoir/Transaction.java
+++ devoir1/src/main/java/com/devoir/Transaction.java
@@ -1,4 +1,6 @@
 package com.devoir;
+import java.lang.reflect.Array;
+import java.util.HashSet;
 import java.util.Date;
 import lombok.Getter;
 import java.util.Set;
@@ -10,10 +12,15 @@ public class Transaction {
     private final Date timestamp;
     private final String reference;
     private final Set<Compte> comptes;
-    public Transaction(Set<Compte> comptes){
+    public Transaction(Compte compteDebit, Compte compteCredit){
         this.reference = UUID.randomUUID().toString();
         this.timestamp = new Date();
-        this.comptes = comptes;
-        
+        this.comptes = new HashSet<Compte>();
+        this.comptes.add(compteDebit);
+        this.comptes.add(compteCredit);
+        if (compteDebit.getBank()==compteCredit.getBank())type = TransactionType.VIREST;
+        else if (compteDebit.getBank().getId()==compteCredit.getBank().getId())type = TransactionType.VIRIN;
+        else if (compteDebit.getBank().getPays()==compteCredit.getBank().getPays())type = TransactionType.VIRMULTA;
+        else type = TransactionType.VIRCHA;
     }
 }
